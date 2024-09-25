2c0a0b709887a4cfa167a65eddaf343807d669af
Pedro Henrick <pedrohenrickqa@gmail.com>
2024-09-24 23:02:52 -0300

    feat: add copyright and scroll to top button

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 const mainDiv = document.querySelector('main');
 const menuLinks = document.querySelectorAll('.menu a');
+const toTopButton = document.querySelector('.to-top')
 
-const getDistanceFromTheTop = (element) => {
-    const id = element.getAttribute('href');
+const getDistanceFromTheTop = (el) => {
+    const id = el.getAttribute('href');
     return document.querySelector(id).offsetTop;
 }
 
-const scrollToSection = (event) => {
-    event.preventDefault();
+const scrollToSection = (e) => {
+    e.preventDefault();
     mainDiv.scroll({
-        top: getDistanceFromTheTop(event.target),
+        top: getDistanceFromTheTop(e.target),
         behavior: 'smooth'
     })
 }
@@ -17,3 +18,26 @@ const scrollToSection = (event) => {
 menuLinks.forEach(link => {
     link.addEventListener('click', scrollToSection);
 })
+
+toTopButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    mainDiv.scroll({
+        top: 0,
+        behavior: 'smooth'
+    })
+})
+
+mainDiv.addEventListener('scroll', (e) => {
+    e.preventDefault();
+    const heroDiv = document.querySelector('.hero');
+    const heroDistanceFromTheTop = heroDiv.getBoundingClientRect()['top'];
+
+    if (heroDistanceFromTheTop > (window.innerHeight - 1) * -1) {
+        toTopButton.classList.add('to-top-hidden');
+        return;
+    }
+
+    if (heroDistanceFromTheTop < 0) {
+        toTopButton.classList.remove('to-top-hidden');
+    }
+});
